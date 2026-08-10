d24ed94e9f667fe16e28c3574f0f74a32101fd87
ISHAN GOEL <ishangoel10408@gmail.com>
2026-08-10 13:03:51 +0530

    Add files via upload

---
+++ day1_code1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+c=a+b;
+
+    printf("Sum of two numbers are %d",c);
+    return 0;
+}
