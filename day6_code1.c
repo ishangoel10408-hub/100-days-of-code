50d77791b5bedf5f6153c39c7c868a169bb9ea65
Ishan <ishangoel10408@gmail.com>
2026-08-15 00:40:01 +0530

    Fix Day 6 code

---
+++ day6_code1.c
@@ -0,0 +1,32 @@
+// Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0) {
+        printf("Even Number");
+    } else {
+        printf("Odd Number");
+    }
+
+    return 0;
+}
