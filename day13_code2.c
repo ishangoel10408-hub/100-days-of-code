58f74ba558548d505e8f430d51138b9c8f013adb
Ishan <ishangoel10408@gmail.com>
2026-08-22 22:08:56 +0530

    DAY 13

---
+++ day13_code2.c
@@ -0,0 +1,19 @@
+// Question 26
+// Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+
+    for (i = 1 ;i <= n ; i++)
+    {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
