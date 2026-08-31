020ed4f7763a65522e8d8bf31b139dc5821c7beb
Ishan <ishangoel10408@gmail.com>
2026-09-01 00:45:37 +0530

    DAY 23

---
+++ DAY 21-30/day23_code2.c
@@ -0,0 +1,24 @@
+// Q46: Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, rows = 5;
+
+    for (i = 0; i < rows; i++)
+    {
+
+        for (j = 0; j < rows; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+     return 0;
+}
