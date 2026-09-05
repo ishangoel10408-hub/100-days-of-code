848740455de0237bf2524268c8c1674043c87179
Ishan <ishangoel10408@gmail.com>
2026-09-06 00:25:50 +0530

    DAY 28

---
+++ DAY 21-30/day28_code2.c
@@ -0,0 +1,17 @@
+// Question 56
+// Read and print elements of a one-dimensional array.
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    for (i=0; i<10; i++)
+
+    printf("%d ", arr[i]);
+
+    return 0;
+}
