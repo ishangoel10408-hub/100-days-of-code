06faa116b92923948faa17fd6db25c88acafdb2e
Ishan <ishangoel10408@gmail.com>
2026-09-07 00:05:04 +0530

    DAY 29

--- DAY 21-30/day29_code2.c
+++ DAY 21-30/day29_code2.c
@@ -1,3 +1,25 @@
 // Question 58
 // Find the maximum and minimum element in an array.
 
+#include <stdio.h>
+
+int main(){
+    int i,largest = 0,smallest ;
+
+    int arr[5]= {10,45,3,57,27};
+
+    for (i=0;i<5;i++)
+    {
+        if(arr[i]>largest)
+        largest=arr[i];
+
+        if(arr[i]<smallest)
+        smallest=arr[i];
+    }
+
+    printf("Largest Array = %d\n", largest);
+    printf("Smallest Array = %d\n", smallest);
+
+
+    return 0;
+}
