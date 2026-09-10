b830b5627379ed00f8eb850a530a30675572eb58
Ishan <ishangoel10408@gmail.com>
2026-09-10 12:08:18 +0530

    DAY 32

---
+++ day32_code1.c
@@ -0,0 +1,17 @@
+// Question 63
+// Merge two arrays.
+
+#include <stdio.h>
+
+int main() {
+    int a[3] = {1, 2, 3};
+    int b[3] = {4, 5, 6};
+
+    for(int i = 0; i < 3; i++)
+        printf("%d ", a[i]);
+
+    for(int i = 0; i < 3; i++)
+        printf("%d ", b[i]);
+
+    return 0;
+}
