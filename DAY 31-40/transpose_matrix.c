871e5a3aa47914cf7f09def95b01e1f2154f1915
Ishan <ishangoel10408@gmail.com>
2026-09-16 10:43:17 +0530

    DAY 37

---
+++ DAY 31-40/transpose_matrix.c
@@ -0,0 +1,24 @@
+// Question 74
+// Find the transpose of a matrix.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], r, c, i, j;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    for(i = 0; i < c; i++)
+    {
+        for(j = 0; j < r; j++)
+            printf("%d ", a[j][i]);
+        printf("\n");
+    }
+
+    return 0;
+}
