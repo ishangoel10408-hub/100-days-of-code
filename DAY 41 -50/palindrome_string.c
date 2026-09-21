4cf187e55083647e22fba3a17c23ed3e8f25e6bd
Ishan <ishangoel10408@gmail.com>
2026-09-21 11:20:50 +0530

    String Palindrome

---
+++ DAY 41 -50/palindrome_string.c
@@ -0,0 +1,34 @@
+// Q86 (Strings)
+// Check if a string is a palindrome.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, length = 0, flag = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++)
+    {
+        if (str[i] != str[length - 1 - i])
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
