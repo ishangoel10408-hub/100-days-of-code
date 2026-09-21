9d969e04dc5e87b6dc4b0310f3a75a7a8f47208d
Ishan <ishangoel10408@gmail.com>
2026-09-21 11:14:29 +0530

    VOWELS AND CONSONANTS IN STRING

---
+++ DAY 41 -50/vowels_consonants.c
@@ -0,0 +1,32 @@
+// Count vowels and consonants in a string.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, vowels = 0, consonants = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            vowels++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            consonants++;
+        }
+
+        i++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d", consonants);
+
+    return 0;
+}
