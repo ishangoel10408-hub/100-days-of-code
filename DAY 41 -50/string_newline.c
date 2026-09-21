dd4c92f860411ab302f75534e718b7d8f5294673
Ishan <ishangoel10408@gmail.com>
2026-09-21 11:12:14 +0530

    string on a new line

---
+++ DAY 41 -50/string_newline.c
@@ -0,0 +1,21 @@
+// Q82 (Strings) 
+// Print each character of a string on a new line.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    while (str[i] != '\0')
+    {
+        printf("%c\n", str[i]);
+        i++;
+    }
+
+    return 0;
+}
